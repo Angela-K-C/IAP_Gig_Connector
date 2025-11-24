3029ab701d646ac3cce987ad6a6ce56e5857cb7a
Wendy <wendywendo06@gmail.com>
2025-11-24 16:32:34 +0300

    Migration issues

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,9 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            
-            // This is the CRITICAL line you need
-            $table->enum('role', ['student', 'provider', 'admin'])->default('student'); 
+            $table->string('role');
 
             $table->rememberToken();
             $table->timestamps();
